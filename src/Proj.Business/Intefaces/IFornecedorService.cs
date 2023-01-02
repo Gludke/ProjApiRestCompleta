ebe9fa1caf081acce6c9686df1007574dcdb92ea
Guilherme Ludke <guiludke@gmail.com>
2023-01-02 12:25:18 -0300

    Adicionar arquivos de projeto.

---
+++ src/Proj.Business/Intefaces/IFornecedorService.cs
@@ -0,0 +1,13 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Intefaces
+{
+    public interface IFornecedorService : IDisposable
+    {
+        Task<bool> Adicionar(Fornecedor fornecedor);
+        Task<bool> Atualizar(Fornecedor fornecedor);
+        Task<bool> Remover(Guid id);
+
+        Task AtualizarEndereco(Endereco endereco);
+    }
+}
