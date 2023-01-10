ca4d19daef27da9243f53f6cee8e529cfa685f6e
Guilherme Ludke <guilherme.ludke@magnatainvestimentos.com.br>
2023-01-10 19:55:04 -0300

    aa

--- src/Proj.Api/ViewModels/Produto/AddProdutoViewModel.cs
+++ src/Proj.Api/ViewModels/Produto/AddProdutoViewModel.cs
@@ -15,8 +15,10 @@ namespace Proj.Api.ViewModels.Produto
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string ImagemUpload { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Imagem { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
