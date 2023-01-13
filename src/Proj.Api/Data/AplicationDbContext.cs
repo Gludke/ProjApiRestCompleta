9dca48c4aa1a147c29a29828f925dfe539d92436
Guilherme Ludke <guilherme.ludke@magnatainvestimentos.com.br>
2023-01-13 00:37:27 -0300

    aa

--- src/Proj.Api/Data/AplicationDbContext.cs
+++ src/Proj.Api/Data/AplicationDbContext.cs
@@ -3,8 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Proj.Api.Data
 {
+    //Add package 'Microsoft.AspNetCore.Identity.EntityFrameworkCore'
+
     // Criamos um novo contexto de acesso à dados por causa da herança de 'IdentityDbContext', mas isso
-    //ñão exigirá a criação de um novo BD no sistema, pois a connections string é a mesma nesse caso.
+    //não exigirá a criação de um novo BD no sistema, pois a connections string é a mesma nesse caso.
     public class AplicationDbContext : IdentityDbContext
     {
         public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) { }
